729505e9bc150acae3f8021ba2addda164033398
Greg Doud <gpdoud@gmail.com>
2021-09-24 12:30:11 -0400

    Add project files.

---
+++ OddOrEvent/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OddOrEvent
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Enter an integer: ");
+			var answer = Console.ReadLine();
+			var value = Convert.ToInt32(answer);
+
+
+
+			if(value % 2 == 0)
+			{
+				Console.WriteLine("The number is even");
+			} else
+			{
+				Console.WriteLine("The number is odd");
+			}
+		}
+	}
+}
